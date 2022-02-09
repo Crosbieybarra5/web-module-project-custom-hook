da0728c92de9beaecd9e0a8559398a6a121a9f38
unknown <crosbieybarra5@gmail.com>
2022-02-09 17:55:17 -0600

    test

---
+++ src/useDarkMode.js
@@ -0,0 +1,7 @@
+import useLocalStorage from './useLocalStorage'
+
+
+export const useDarkMode = initialValue => {
+    const [someValue, setSomeValue] = useLocalStorage('values', initialValue)
+    return [someValue, setSomeValue];
+}
